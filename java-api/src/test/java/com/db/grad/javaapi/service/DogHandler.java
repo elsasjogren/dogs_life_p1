ef92b2ac09ac5e9a6c7c30d5b40b0c516c43d473
elsasjogren <elsasjogren5@gmail.com>
2023-07-27 21:03:42 +0100

    Finialised all tests for Dog Handler and passed

--- java-api/src/test/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,6 +3,10 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class DogHandler {
     private DogsRepository itsDogsRepo;
     public DogHandler(DogsRepository itsDogRepo) {
@@ -18,19 +22,44 @@ public class DogHandler {
     }
 
     Dog getDogByName( String name ){
+        //should return a null if the dog does not exist
+        //if several dogs exist with the same name, it should return a null, otherwise it returns the single Dog object
         Dog newDog = new Dog();
         newDog.setName(name);
-        int count = itsDogsRepo.findByName(newDog).size();
-        if(count > 1){
-            return null;
-        }
-        else if (count == 1){
+        //create a list of dog objects
+        List<Dog> dogsList = itsDogsRepo.findByName(newDog);
+        int count = dogsList.size();
+        if (count == 1){
             return itsDogsRepo.findByName(newDog).get(0);
         }
         else{
             return null;
         }
     }
+
+    public Dog getDogById( long id ){
+        //should return a null if the dog does not exist
+        return itsDogsRepo.findById(id);
+    }
+
+    long updateDogDetails( Dog dog ){
+        //should return the updated Dog’s ID
+        return  itsDogsRepo.save(dog);
+    }
+
+    boolean removeDog( long id ){
+        //should return true if the dog was removed
+        //use optionals to access a specific method
+        boolean result = false;
+        Optional<Dog> theDog = Optional.ofNullable(itsDogsRepo.findById(id));
+        if(theDog.isPresent()){
+            itsDogsRepo.delete(theDog.get());
+            result = true;
+        }
+        return result;
+    }
+
+
 
 
 }
--- java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -35,7 +35,114 @@ public class DogsHandlerTest {
 
     @Test
     public  void    add_several_dogs_return_number_of_dogs_match_number_added(){
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bear");
+        cut.addDog( theDog );
+
+        Dog theDog1 = new Dog();
+        theDog1.setName("Peanut");
+        cut.addDog( theDog1 );
+
+        Dog theDog2 = new Dog();
+        theDog2.setName("Jonathon");
+        cut.addDog( theDog2 );
+
+        Dog theDog3 = new Dog();
+        theDog3.setName("Happy");
+        cut.addDog( theDog3 );
+
+        Dog theDog4 = new Dog();
+        theDog4.setName("Luna");
+        cut.addDog( theDog4 );
+
+        Dog theDog5 = new Dog();
+        theDog5.setName("Scamp");
+        cut.addDog( theDog5 );
+
+        Dog theDog6 = new Dog();
+        theDog6.setName("Joy");
+        cut.addDog( theDog6 );
+
+        Dog theDog7 = new Dog();
+        theDog7.setName("Winston");
+        cut.addDog( theDog7 );
+
+        Dog theDog8 = new Dog();
+        theDog8.setName("Link");
+        cut.addDog( theDog8 );
+        Dog theDog9 = new Dog();
+        theDog9.setName("Brother");
+        cut.addDog( theDog9 );
+        Dog theDog10 = new Dog();
+        theDog10.setName("Scofield");
+        cut.addDog( theDog10 );
+        Dog theDog11 = new Dog();
+        theDog11.setName("Simba");
+        cut.addDog( theDog11 );
+        Dog theDog12 = new Dog();
+        theDog12.setName("Dog");
+        cut.addDog( theDog12 );
+        int expectedResult = 13;
+        //act
+        long actualResult = cut.getNoOfDogs();
+        //assert
+        assertEquals( expectedResult, actualResult );
+    }
+
+    //write tests for 4 new methods
+    @Test
+    public  void testGetDogByName(){
+        //arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bean");
+        cut.addDog( theDog );
+        Dog expectedResult = theDog;
+        Dog result = cut.getDogByName("Bean");
+        //act
+        //assert
+        assertEquals( expectedResult, result );
 
     }
+
+    @Test
+    public  void testGetDogById(){
+        //arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setId(12);
+        cut.addDog( theDog );
+        Dog expectedResult = theDog;
+        Dog result = cut.getDogById(theDog.getId());
+        //act
+        //assert
+        assertEquals( expectedResult, result );
+    }
+
+    @Test
+    public  void testUpdateDogDetails(){
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setId(12);
+        cut.addDog( theDog );
+        //theDog.updateDogDetails
+        Dog theDog2 = new Dog();
+        theDog2.setId(2);
+        long id = cut.updateDogDetails(theDog2);
+        assertEquals( 2, id );
+    }
+
+    @Test
+    public  void testRemoveDog(){
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setId(12);
+        cut.addDog( theDog );
+        boolean result = cut.removeDog(theDog.getId());
+        Dog expectedResult = theDog;
+        assertEquals( true, result );
+    }
+
 
 }
